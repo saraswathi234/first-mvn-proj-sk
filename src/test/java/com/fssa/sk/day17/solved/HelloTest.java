41561ee508ba46f4fc5c2025e19fb188610a1212
saraswathi234 <116253625+saraswathi234@users.noreply.github.com>
2023-06-23 10:48:20 +0530

    day17 update

---
+++ src/test/java/com/fssa/sk/day17/solved/HelloTest.java
@@ -0,0 +1,19 @@
+package com.fssa.sk.day17.solved;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+
+public class HelloTest {
+	
+	
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+		
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+}
