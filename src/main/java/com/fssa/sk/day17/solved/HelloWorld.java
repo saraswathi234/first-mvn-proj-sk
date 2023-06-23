d5066f5b483bda59d73324db0ff65876c48cb9fe
saraswathi234 <116253625+saraswathi234@users.noreply.github.com>
2023-06-23 10:25:26 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/sk/day17/solved/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fssa.sk.day17.solved;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		//print the hello world program
+		System.out.println("Hello World");
+	}
+	}
+
